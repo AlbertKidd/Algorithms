6079baef16ea4ae96a1009c9731067822c4b4935
Albert Kidd <475555354@qq.com>
2016-06-09 22:43:00 +0800

    add q1-1-20

--- src/Chapter_1_Fundamentals/Q1_1_20.java
+++ src/Chapter_1_Fundamentals/Q1_1_20.java
@@ -1,5 +1,20 @@
 package Chapter_1_Fundamentals;
 
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+//编写一个递归的静态方法计算ln(N!) 的值。
+
 public class Q1_1_20 {
+	public static void main(String[] args){
+		int i = StdIn.readInt();
+		StdOut.println(factorialLn1(i));
+	}
 	
+	public static double factorialLn1(int N){
+		if(N > 1)
+			return Math.log(N) + factorialLn1(N-1);
+		else
+			return 0;
+	}
 }
