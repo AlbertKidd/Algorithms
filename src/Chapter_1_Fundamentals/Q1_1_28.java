3c53335b4fe3e7604727ec4e68b7ede3c8d616ce
Albert Kidd <475555354@qq.com>
2016-06-12 21:52:11 +0800

    add 1.1.28

---
+++ src/Chapter_1_Fundamentals/Q1_1_28.java
@@ -0,0 +1,26 @@
+package Chapter_1_Fundamentals;
+
+import java.util.ArrayList;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Q1_1_28 {
+	
+	public static void main(String[] args){
+		int[] a = {1, 1, 2, 3, 3, 45, 545, 645, 645, 675};
+		StdOut.print(elimateDuplication(a));
+	}
+	
+	public static ArrayList<Integer> elimateDuplication(int[] a){
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		int temp = a[0];
+		list.add(a[0]);
+		for(int i=0; i<a.length; i++){
+			if(a[i] != temp){
+				temp = a[i];
+				list.add(temp);
+			}
+		}
+		return list;
+	}
+}
