b059837da7461576b8935c0083be3a3070337e0d
Albert Kidd <475555354@qq.com>
2016-06-13 21:45:11 +0800

    add 1.1.29~31

---
+++ src/Chapter_1_Fundamentals/Q1_1_29.java
@@ -0,0 +1,36 @@
+package Chapter_1_Fundamentals;
+
+import edu.princeton.cs.algs4.StdOut;
+
+//等值键。
+//为BinarySearch 类添加一个静态方法rank()，它接受一个键和一个整型有序数组（可能存在重复键）作为参数并返回数组中小于该键的元素数量，以及一个类似的方法count() 来返回数组中等于该键的元素的数量。
+//注意：如果i 和j 分别是rank(key,a) 和count(key,a)的返回值，那么a[i..i+j-1] 就是数组中所有和key 相等的元素。
+
+public class Q1_1_29 {
+	public static void main(String[] args){
+		int[] a = {1, 1, 2 , 3 ,3 , 3 , 4, 5 ,6 ,7 };
+		int i = rank(1, a);
+		StdOut.println(i);
+		int j = count(1, a);
+		StdOut.println(j);
+		for(int x = i; x < i+j; x++){
+			StdOut.println(x);
+		}
+	}
+	public static int rank(int key, int[] a){
+		int n = 0;
+		for(int i : a){
+			if(a[i] < a[key])
+				n++;
+		}
+		return n;
+	}
+	public static int count(int key, int[] a){
+		int n = 0;
+		for(int i : a){
+			if(a[i] == a[key])
+				n++;
+		}
+		return n;
+	}
+}
---
+++ src/Chapter_1_Fundamentals/Q1_1_30.java
@@ -0,0 +1,30 @@
+package Chapter_1_Fundamentals;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+//数组练习。编写一段程序，创建一个N×N 的布尔数组a[][]。其中当i 和j 互质时（没有相同因子），a[i][j] 为true，否则为false。
+
+public class Q1_1_30 {
+	public static void main(String[] args){
+		int N = StdIn.readInt();
+		boolean[][] a = new boolean[N][N];
+		for(int i=0; i<N; i++){
+			for(int j=0; j<N; j++){
+				if(gcd(i,j) == 1)
+					a[i][j] = true;
+				else a[i][j] = false;
+				StdOut.print(a[i][j] + "  ");
+			}
+			StdOut.println();
+		}
+	}
+	public static int gcd(int p, int q){
+		if(p == 0 || q == 0)
+			return 1;
+		if(p % q == 0)
+			return q;
+		else
+			return gcd(q, p%q);
+	}
+}
