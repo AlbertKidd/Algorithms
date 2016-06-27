08029c07ffb2dd7aff2e6c8be1129d9d5e60d988
Albert Kidd <475555354@qq.com>
2016-06-27 16:08:42 +0800

    add

---
+++ Chapter1_Fundamentals/c1_3_bags_queues_stacks/Q1_3_5.java
@@ -0,0 +1,19 @@
+package c1_3_bags_queues_stacks;
+
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Q1_3_5 {
+	public static void main(String[] args){
+		StdOut.print(toBinary(StdIn.readInt()));
+	}
+	public static String toBinary(int N){
+		Stack<Integer> stack = new Stack<Integer>();
+		while(N > 0){
+			stack.push(N % 2);
+			N /= 2;
+		}
+		return stack.toString();
+	}
+}
