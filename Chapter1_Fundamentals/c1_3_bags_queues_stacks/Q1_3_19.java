90ada65d3db29d960b65d9d75be50c3e3c6aabb1
Albert Kidd <475555354@qq.com>
2016-07-01 09:18:22 +0800

    add

---
+++ Chapter1_Fundamentals/c1_3_bags_queues_stacks/Q1_3_19.java
@@ -0,0 +1,76 @@
+package c1_3_bags_queues_stacks;
+
+import java.util.NoSuchElementException;
+
+import edu.princeton.cs.algs4.Queue;
+
+public class Q1_3_19 {
+	public static void main(Queue q){
+		
+	}
+	
+	class AQueue<Item>{
+		private Node<Item> first;
+		private Node<Item> last;
+		private int N;
+		
+		private class Node<Item>{
+			private Item item;
+			private Node<Item> next;
+		}
+		
+		public AQueue(){
+			first = null;
+			last = null;
+			N = 0;
+		}
+		
+		public boolean isEmpty(){
+			return first == null;
+		}
+		
+		public int size(){
+			return N;
+		}
+		
+		public void enqueue(Item item){
+			Node<Item> oldLast = last;
+			last = new Node<Item>();
+			last.item = item;
+			last.next = null;
+			if(isEmpty())
+				first = last;
+			else
+				oldLast.next = last;
+			N++;
+		}
+		
+		public Item dequeue(){
+			if(isEmpty())
+				throw new NoSuchElementException("Stack under flow");
+			Item item = first.item;
+			first = first.next;
+			N--;
+			if(isEmpty())
+				last = null;
+			return item;
+		}
+		
+		public Item dequeueLast(){
+			if(isEmpty())
+				throw new NoSuchElementException("Stack under flow");
+			Item mItem = null;
+			for(Node node = first; node.next != null; node = node.next){
+				if(node.next.next == null){
+					mItem = last.item;
+					last.item = null;
+					last = node;
+					N--;
+					if(isEmpty())
+						first = null;
+				}
+			}
+			return mItem;
+		}
+	}
+}
