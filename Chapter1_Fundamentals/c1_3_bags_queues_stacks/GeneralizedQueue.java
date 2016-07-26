177d022e21b2ebf9b7b7e9a4a0938109339e8be0
Albert Kidd <475555354@qq.com>
2016-07-26 22:10:37 +0800

    add

---
+++ Chapter1_Fundamentals/c1_3_bags_queues_stacks/GeneralizedQueue.java
@@ -0,0 +1,52 @@
+package c1_3_bags_queues_stacks;
+
+import java.util.NoSuchElementException;
+
+public class GeneralizedQueue<Item> {
+	private int N;
+	private Item[] q;
+	private int first,last;
+	
+	public GeneralizedQueue(){
+		N = 0;
+		q = (Item[]) new Object[1];
+		first = 0;
+		last = 0;
+	}
+	
+	public boolean isEmpty(){
+		return N == 0;
+	}
+	
+	public void insert(Item x){
+		if(N == q.length)
+			resize(2 * N);
+		q[last++] = x;
+		N++;
+	}
+	
+	public Item delete(int k){
+		if(k >= N)
+			throw new NoSuchElementException();
+		resize(N);
+		Item item = q[k - 1];
+		for(int i = 0; i < N; i++){
+			if(i == N - 1)
+				q[i] = null;
+			else if(i > k - 1)
+				q[i] = q[i + 1];
+		}
+		N--;
+		return item;
+	}
+	
+	private void resize(int max){
+		assert max >= N;
+		Item[] temp = (Item[])new Object[max];
+		for(int i = 0; i < N; i++)
+			temp[i] = q[i + first];
+		q = temp;
+		first = 0;
+		last = N;
+	}
+}
