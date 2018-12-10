6da9df934ca1ad78e5eacfa6c15620eff74b56ad
kidd <475555354@qq.com>
2018-12-10 23:14:42 +0800

    commit for switch env

--- src/main/java/c1_fundamentals/c1_1_programming_model/Q1_1_22.java
+++ src/main/java/c1_fundamentals/c1_1_programming_model/Q1_1_22.java
@@ -1,40 +1,109 @@
 package c1_fundamentals.c1_1_programming_model;
 
-
-//使用1.1.6.4 节中的rank() 递归方法重新实现BinarySearch 并跟踪该方法的调用。
-//每当该方法被调用时，打印出它的参数lo 和hi 并按照递归的深度缩进。
-//提示：为递归方法添加一个参数来保存递归的深度。
-
+import common.Printer;
+import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
 
+/**
+ * 使用1.1.6.4节中的rank() 递归方法重新实现BinarySearch 并跟踪该方法的调用。
+ * 每当该方法被调用时，打印出它的参数lo 和hi 并按照递归的深度缩进。
+ * 提示：为递归方法添加一个参数来保存递归的深度。
+ */
 public class Q1_1_22 {
-	static String s = "";
+
 	public static void main(String[] args){
-		int[] a = {1, 21, 25, 34, 46, 56, 67};
-		int b = rank(46, a);
-		StdOut.print(b);
+		StdOut.println("请输入初始数组的大小：");
+		int size = StdIn.readInt();
+		int[] array = initArray(size);
+		StdOut.println("初始数组为：");
+		Printer.print(array);
+		StdOut.println("请输入要查找的整数：");
+		int num = StdIn.readInt();
+		rank(array, num);
 	}
 
-	private static int rank(int key, int[] a){
-		return rank(key, a, 0, a.length-1);
-	}
-
-	private static int rank(int key, int[] a, int lo, int hi){
-		StdOut.println(s + lo + "," + hi);
-		s += "  ";
-		if(lo > hi){
-			StdOut.print(s);
-			return -1;
+	/**
+	 * 根据传入的数字构造一个对应大小的数组，其中元素为递增排列
+	 * @param size
+	 * @return
+	 */
+	public static int[] initArray(int size){
+		int[] arr = new int[size];
+		arr[0] = StdRandom.uniform(10);
+		for (int i = 1; i < size; i++){
+			arr[i] = arr[i - 1] + StdRandom.uniform(1, 10);
 		}
-		
-		int mid = lo + (hi - lo)/2;
-		if(key > a[mid])
-			return rank(key, a, mid+1, hi);
-		else if(key < a[mid])
-			return rank(key, a, lo, mid-1);
-		else {
-			StdOut.print(s);
-			return mid;
+		return arr;
+	}
+
+	/**
+	 * 二分查找，打印查找深度
+	 * @param arr
+	 * @param key
+	 * @return
+	 */
+	private static int rank(int[] arr, int key){
+		int low = 0;
+		int high = arr.length;
+		int depth = 0;
+		while (high >= low){
+			StdOut.printf("当前深度：%3s，下限位：%3s，上限位：%3s%n", depth++, low, high);
+			int mid = (high + low) / 2;
+			if (key == arr[mid]){
+				StdOut.printf("%s在数组中为第%s位%n", key, mid);
+				return mid;
+			}else if (key > arr[mid]){
+				low = mid + 1;
+			}else {
+				high = mid - 1;
+			}
+		}
+		StdOut.printf("数组中不存在此值：%s%n", key);
+		return -1;
+	}
+
+	/**
+	 * 另一种解决方案
+	 */
+	static class AnotherSolution{
+
+		/**
+		 * 查找深度
+		 */
+		private static int depth;
+
+		public static void main(String[] args){
+			StdOut.println("请输入初始数组的大小：");
+			int size = StdIn.readInt();
+			int[] array = initArray(size);
+			StdOut.println("初始数组为：");
+			Printer.print(array);
+			StdOut.println("请输入要查找的整数：");
+			int num = StdIn.readInt();
+			rank(array, num);
+		}
+
+		private static int rank(int[] arr, int key){
+			return rank(arr, key, 0, arr.length - 1);
+		}
+
+		private static int rank(int[] arr, int key, int low, int high){
+			if (high >= low){
+				StdOut.printf("当前深度：%3s，下限位：%3s，上限位：%3s%n", depth++, low, high);
+				int mid = (low + high) / 2;
+				if (key == arr[mid]){
+					StdOut.printf("%s在数组中为第%s位", key, mid);
+					return mid;
+				}else if (key > arr[mid]){
+					low = mid + 1;
+				}else {
+					high = mid - 1;
+				}
+				return rank(arr, key, low, high);
+			}
+			StdOut.printf("数组中不存在此值：%s%n", key);
+			return -1;
 		}
 	}
 }
