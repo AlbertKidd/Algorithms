4098b22f925515d3c493ba2f0500474a0545c9cb
kidd <475555354@qq.com>
2018-12-22 12:17:01 +0800

    optimized code

--- src/main/java/c1_fundamentals/c1_1_programming_model/Q1_1_29.java
+++ src/main/java/c1_fundamentals/c1_1_programming_model/Q1_1_29.java
@@ -1,75 +1,30 @@
 package c1_fundamentals.c1_1_programming_model;
 
-
-//等值键。
-//为BinarySearch 类添加一个静态方法rank()，它接受一个键和一个整型有序数组（可能存在重复键）作为参数并返回数组中小于该键的元素数量，以及一个类似的方法count() 来返回数组中等于该键的元素的数量。
-//注意：如果i 和j 分别是rank(key,a) 和count(key,a)的返回值，那么a[i..i+j-1] 就是数组中所有和key 相等的元素。
-
-
+import common.BinarySearchEx;
+import common.RandomArray;
+import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
+import java.util.Arrays;
+
+/**
+ * 1.1.29
+ * 等值键。
+ * 为BinarySearch类添加一个静态方法rank()，
+ * 它接受一个键和一个整型有序数组（可能存在重复键）作为参数并返回数组中小于该键的元素数量，
+ * 以及一个类似的方法count()来返回数组中等于该键的元素的数量。
+ * 注意：如果i和j分别是rank(key,a)和count(key,a)的返回值，那么a[i..i+j-1]就是数组中所有和key相等的元素。
+ */
 public class Q1_1_29 {
+
 	public static void main(String[] args){
-		int[] a = {1, 1, 2 , 3 ,3 , 3 , 4, 5 ,6 ,7 };
-		int i = rank(4, a);
-		StdOut.println(i);
-		int j = count(4, a);
-		StdOut.println(j);
-	}
-	private static int rank(int key, int[] a){
-		int nearKey = searchNearKey(a, key);
-		if (key > a[nearKey]){
-			return nearKey + 1;
-		}else if (key < a[nearKey]){
-			return nearKey;
-		}else {
-			while (nearKey >= 0 && key == a[nearKey]) nearKey--;
-			return nearKey + 1;
-		}
+		StdOut.println("请输入初始数组的大小：");
+		int[] arr = RandomArray.generate(StdIn.readInt(), true, true);
+		StdOut.printf("生成的数组为：%n%s%n", Arrays.toString(arr));
+		StdOut.println("请输入需要搜索的数值：");
+		int value = StdIn.readInt();
+		StdOut.printf("rank函数结果为: %s%n", BinarySearchEx.rank(arr, value));
+		StdOut.printf("count函数结果为: %s%n", BinarySearchEx.count(arr, value));
 	}
 
-	private static int count(int key, int[] a){
-		int searchedKey = searchKey(a, key);
-		if (searchedKey == -1){
-			return 0;
-		}else {
-			int n = 1;
-			int pre = searchedKey - 1;
-			while (pre >= 0 && key == a[pre]){
-				n++;
-				pre = pre - 1;
-			}
-			int after = searchedKey + 1;
-			while (key < a.length && key == a[after] ){
-				n++;
-				after = after + 1;
-			}
-			return n;
-		}
-	}
-
-	private static int searchKey(int[] a, int value){
-		int lo = 0;
-		int hi = a.length;
-		while (lo < hi){
-			int mid = lo + (hi - lo) / 2;
-			if (value > a[mid]) lo = mid + 1;
-			else if (value < a[mid]) hi = mid - 1;
-			else return mid;
-		}
-		return -1;
-	}
-
-	private static int searchNearKey(int[] a, int value){
-		int lo = 0;
-		int hi = a.length;
-		int mid = 0;
-		while (lo < hi){
-			mid = lo + (hi - lo) / 2;
-			if (value < a[mid]) hi = mid - 1;
-			else if (value > a[mid]) lo = mid + 1;
-			else return mid;
-		}
-		return mid;
-	}
 }
--- src/main/java/common/BinarySearchEx.java
+++ src/main/java/common/BinarySearchEx.java
@@ -3,7 +3,7 @@ package common;
 import edu.princeton.cs.algs4.BinarySearch;
 
 /**
- * 二分查找扩展
+ * 二分查找扩展类
  * 由于BinarySearch构造器为私有，无法继承
  */
 public class BinarySearchEx{
@@ -29,5 +29,58 @@ public class BinarySearchEx{
             return indexOf(arr, key, start, end);
         }
         return -1;
+    }
+
+    /**
+     * 找到数组中小于key值的数量
+     * @param arr
+     * @param key
+     * @return
+     */
+    public static int rank(int[] arr, int key){
+        int rank = 0;
+        int min = 0;
+        int max = arr.length;
+        while (min < max){
+            int mid = (min + max) / 2;
+            if (key < arr[mid]){
+                max = mid - 1;
+            }else if (key > arr[mid]){
+                rank = mid + 1;
+                min = mid + 1;
+            }else {
+                rank = mid;
+                while (rank > 0 && arr[rank] == arr[rank - 1]){
+                    rank--;
+                }
+                return rank;
+            }
+        }
+        return rank;
+    }
+
+    /**
+     * 找出数组中与指定值相等的数量
+     * @param arr
+     * @param key
+     * @return
+     */
+    public static int count(int[] arr, int key){
+        int count = 0;
+        int index = BinarySearch.indexOf(arr, key);
+        if (index != -1){
+            count = 1;
+            int low = index;
+            while (low > 0 && arr[low] == arr[low - 1]){
+                low--;
+                count++;
+            }
+            int high = index;
+            while (high < arr.length - 2 && arr[high] == arr[high + 1]){
+                high++;
+                count++;
+            }
+        }
+        return count;
     }
 }
